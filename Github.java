cee98896cddb126acf5229ff2225b1a83168a3eb
DoddaCandrashekar <83276887+DoddaCandrashekar@users.noreply.github.com>
2021-05-09 15:09:38 +0530

    Add files via upload

---
+++ Github.java
@@ -0,0 +1,485 @@
+import java.util.*;
+class APK
+{
+Scanner b=new Scanner(System.in);
+}
+class GUNTUR extends APK
+{
+public void guntur()
+{
+int e;
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+e=b.nextInt();
+switch(e)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF GUNTUR IS : 168.41 km^2");
+System.out.println("POPULATION OF GUNTUR is : 743,354");
+System.out.println("LOCATED IN DISTRICT : GUNTUR");
+System.out.println("ELEVATION : 30m (100 ft)");
+System.out.println("LITERACY : 67.40%");
+System.out.println("REGION: ANDHRA");
+System.out.println("Municipal Commissioner: S.Nagalakhsmi");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("BRUNDAVAN GARDENS");
+System.out.println("GORENTLA BALAJI TEMPLE");
+System.out.println("CHINAKAKANNI");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("            FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("GANDHI PARK");
+System.out.println("NTR.MANASA SAROVAR");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class VIZAG extends APK
+{
+public void vizag()
+{
+int f;
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+f=b.nextInt();
+switch(f)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF VIZAG IS : 540.00 km^2 ");
+System.out.println("POPULATION OF VIZAG is : 2,035,922");
+System.out.println("LOCATED IN DISTRICT : VIZAG");
+System.out.println("ELEVATION : 45m (148 ft)");
+System.out.println("LITERACY : 67.70%");
+System.out.println("REGION: ANDHRA");
+System.out.println("MAYOR: HARI NARAYANAN");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("SIMHACHALAM APPANA TEMPLE");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("            FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("RAMAKRISHNA BEACH");
+System.out.println("VIZAG ZOO");
+System.out.println("VIZAG SHIP YARD");
+System.out.println("BORRA CAVES");
+System.out.println("KAILASA GIRI");
+System.out.println("YARADA BEACH");
+System.out.println("RISHIKONDA BEACH");
+System.out.println("Dolphin's Nose");
+System.out.println("INS Kurusura Submarine Museum");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class KAKINADA extends APK
+{
+public void kakinada()
+{
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+int g;
+System.out.println("");
+g=b.nextInt();
+switch(g)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF KAKINADA IS : 31.51 km^2"); 
+System.out.println("POPULATION OF KAKINADA : 312,538");
+System.out.println("LOCATED IN DISTRICT :EAST GODAVARI");
+System.out.println("ELEVATION : 25m"); 
+System.out.println("LITERACY : 81.23% ");
+System.out.println("REGION: ANDHRA");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("Kotipalli Someswara Temple");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("            FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("Haritha Coconut Country Resort");
+System.out.println("Coringa Wildlife Sanctuary");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class TIRUPATHI extends APK
+{
+public void tirupathi()
+{
+int h;
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+h=b.nextInt();
+switch(h)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF  TRIUPATI IS : 27.44 km^2"); 
+System.out.println("POPULATION OF TRIUPATI : 312,538");
+System.out.println("LOCATED IN DISTRICT :CHITTOOR");
+System.out.println("ELEVATION : 853m (2,799 ft)");
+System.out.println("LITERACY : 73.23% ");
+System.out.println("REGION: Rayalaseema");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("TIRUMALA BALAJI TEMPLE");
+System.out.println("TIRCHANURU PADMAVATHIDEVI TEMPLE");
+System.out.println("ALIVALU MAGAPURAM");
+System.out.println("VARAHASWAMI TEMPLE");
+System.out.println("VARDARAJULU SWAMI TEMPLE");
+System.out.println("ISKAN TEMPLE");
+System.out.println("Sri Kalyana Venkateswaraswami Temple");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("            FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("Sri Venkateswara National Park ");
+System.out.println("Papavinasam Theertham");
+System.out.println("Talakona Waterfall ");
+System.out.println("Sri Venkatesvara Museum");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class CHITTOOR extends APK
+{
+public void chittoor()
+{
+int j;
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+j=b.nextInt();
+switch(j)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF  CHITTOOR IS : 95.97 km^2"); 
+System.out.println("POPULATION OF CHITTOOR :189,332 ");
+System.out.println("LOCATED IN DISTRICT :CHITTOOR");
+System.out.println("ELEVATION : 333.75m (1,094.98 ft)");
+System.out.println("LITERACY : 72.96% ");
+System.out.println("REGION: Rayalaseema");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("Sri Kapileswaraswami Temple");
+System.out.println("Vallimalai Subramanyar Temple");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("             FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("Horsley Hills");
+System.out.println("Tada Falls");
+System.out.println("Kailasakona Waterfalls");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class ONGOLE extends APK
+{
+public void ongole()
+{
+int k;
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+k=b.nextInt();
+switch(k)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF ONGOLE IS : 25.00 km^2");
+System.out.println("POPULATION OF ONGOLE is : 204,746");
+System.out.println("LOCATED IN DISTRICT : PRAKASAM");
+System.out.println("ELEVATION : 20m (70 ft)");
+System.out.println("LITERACY : 83.04 %");
+System.out.println("REGION: ANDHRA");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("Chennakesava Swamy Temple");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("             FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("Mallavaram Dam");
+System.out.println("Chandavaram Excavation Site");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class RAJAHMUNDRY extends APK
+{
+public void rajahmundry()
+{
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+int v;
+v=b.nextInt();
+switch(v)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF RAJAHMUNDRY IS : 44.50 km^2 ");
+System.out.println("POPULATION OF RAJAHMUNDRY is : 341,831");
+System.out.println("LOCATED IN DISTRICT : EAST GODAVARI");
+System.out.println("ELEVATION : 14m (46 ft)");
+System.out.println("LITERACY : 84.12%");
+System.out.println("REGION: ANDHRA");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("ISKCON Rajahmundry, Sri Sri Radha Gopinath Temple");
+System.out.println("Adurru");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("             FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("Godavari Bridge");
+System.out.println("Royal Park");
+System.out.println("Pushkar Ghat");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class VIJAYAWADA extends APK
+{
+public void vijayawada()
+{
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+int l;
+l=b.nextInt();
+switch(l)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF  VIJAYAWADA IS : 61.88 km^2"); 
+System.out.println("POPULATION OF VIJAYAWADA : 1,476,931");
+System.out.println("LOCATED IN DISTRICT :KRISHNA");
+System.out.println("ELEVATION : 23 m (75 ft)");
+System.out.println("LITERACY : 81.24 %");
+System.out.println("REGION: ANDHRA");
+System.out.println("MAYOR: KONERU SRIDHAR");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("KANAKADURGA TEMPLE");
+System.out.println("MACHAVARAM HANUMAN TEMPLE");
+System.out.println("Sri Ramalingeswara Swami Vari Devasthanam");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("            FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println(" PVP SQUARE ");
+System.out.println(" LEPL ICON ");
+System.out.println("Bhavani Island Resort");
+System.out.println("Prakasam Barrage");
+System.out.println("ISKCON Vijayawada");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class TENALI extends APK
+{
+public void tenali()
+{
+System.out.println("_____________________________________________________________");
+System.out.println("1.CITY NORMAL INFORMATION \n2.PILIGRIMIC PLACE\n3.FUN SPORTS");
+System.out.println("_____________________________________________________________");
+int m;
+m=b.nextInt();
+switch(m)
+{
+case 1:
+System.out.println("      CITY NORMAL INFORMATION:");
+System.out.println("---------------------------------------");
+System.out.println("AREA OF  TENALI IS : 16.58 km^2"); 
+System.out.println("POPULATION OF TENALI : 164,937");
+System.out.println("LOCATED IN DISTRICT :GUNTUR");
+System.out.println("ELEVATION : 15m (49 ft)");
+System.out.println("LITERACY : 82.75%");
+System.out.println("REGION: ANDHRA");
+System.out.println("Chairperson: K.Tulasi Das");
+System.out.println("---------------------------------------");
+break;
+case 2:
+System.out.println("       PILIGRIMIC PLACE:");
+System.out.println("---------------------------------------");
+System.out.println("VYKUNTAPURAM");
+System.out.println("---------------------------------------");
+break;
+case 3:
+System.out.println("             FUN SPORTS:");
+System.out.println("---------------------------------------");
+System.out.println("CHINARAVURU PARK");
+System.out.println("Uppalapadu Bird Sanctuary");
+System.out.println("---------------------------------------");
+break;
+default :
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+}
+}
+}
+class Github
+{
+public static void main(String [] args)
+{
+for( ; ; )
+{
+Scanner a=new Scanner(System.in);
+int i;
+System.out.println("_____________________________________________________________");
+System.out.println(" ENTER THE NUMBER WHICH CITY INFORMATION REQUIRED FOR YOU"); 
+System.out.println("1.GUNTUR \n2.VIZAG \n3.KAKINADA \n4.TRIUPATI \n5.CHITTOOR \n6.ONGOLE \n7.RAJAHMUNDRY \n8.VIJAYAWADA \n9.TENALI \n0.EXIT");
+System.out.println("_____________________________________________________________");
+APK ab=new APK();
+GUNTUR ab1=new GUNTUR();
+VIJAYAWADA ab8=new VIJAYAWADA();
+TENALI ab9=new TENALI();
+RAJAHMUNDRY ab7=new RAJAHMUNDRY();
+ONGOLE ab6=new ONGOLE();
+CHITTOOR ab5=new CHITTOOR();
+TIRUPATHI ab4=new TIRUPATHI();
+KAKINADA ab3=new KAKINADA();
+VIZAG ab2=new VIZAG();
+i=a.nextInt();
+switch(i)
+{
+case 1:
+ab1.guntur();
+break;
+case 2:
+ab2.vizag();
+break;
+case 3:
+ab3.kakinada();
+break;
+case 4:
+ab4.tirupathi();
+break;
+case 5:
+ab5.chittoor();
+break;
+case 6:
+ab6.ongole();
+break;
+case 7:
+ab7.rajahmundry();
+break;
+case 8:
+ab8.vijayawada();
+break;
+case 9:
+ab9.tenali();
+break;
+default : 
+System.out.println("---------------------------------------");
+System.out.println("THIS KEY IS NOT AVILABLE:SORRY ");
+System.out.println("---------------------------------------");
+break;
+case 0 :System.exit(0);
+}
+}
+}
+}
